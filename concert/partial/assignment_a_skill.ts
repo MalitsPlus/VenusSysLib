c434f3d8b5ccddd020e86e3c39805ecba30fabd2
MalitsPlus <hua8102259@gmail.com>
2023-01-29 00:31:42 +0900

    fix: a skill assignment

--- concert/partial/assignment_a_skill.ts
+++ concert/partial/assignment_a_skill.ts
@@ -3,12 +3,12 @@ import { Concert } from "../concert";
 import { Indexes } from "../consts/chart_consts";
 
 export default function assignment_a_skill(this: Concert) {
-  // clear current actables
-  this.actables = []
   const current = this.current
   if (current.chartType === MusicChartType.ActiveSkill) {
     if (this.actables.length > 0) {
       if (current.getCardStatus(this.actables[0].index)?.getEffects(SkillEfficacyType.ActiveSkillChanceAssignment).length) {
+        // clear current actables
+        this.actables = []
         const originalPos = current.originalActPosition
         const isOpponent = originalPos > 5 ? true : false
         for (const idx of Indexes.filter(it => isOpponent ? it > 5 : it <= 5)) {
--- main.ts
+++ main.ts
@@ -3,15 +3,15 @@ import simulate from "./index";
 import { getDefaultUserCard } from './satomi/card';
 import { CustomNote, TransDeck } from "./types/trans_types"
 
-const questId = "gvg-quest-019"
+const questId = "marathon-raid-23-0128-quest-4"
 const allyDeck: TransDeck = {
   name: "test deck",
   userCards: [
-    { index: 1, card: getDefaultUserCard("card-kor-05-idol-00") },
-    { index: 2, card: getDefaultUserCard("card-mei-05-fest-00") },
-    { index: 3, card: getDefaultUserCard("card-chs-05-flow-00") },
-    { index: 4, card: getDefaultUserCard("card-szk-05-prem-00") },
-    { index: 5, card: getDefaultUserCard("card-mna-05-vlnt-00") },
+    { index: 1, card: getDefaultUserCard("card-rei-05-rock-00") },
+    { index: 2, card: getDefaultUserCard("card-mna-05-vlnt-00") },
+    { index: 3, card: getDefaultUserCard("card-rei-05-fest-00") },
+    { index: 4, card: getDefaultUserCard("card-ai-05-tact-00") },
+    { index: 5, card: getDefaultUserCard("card-ai-05-mizg-01") },
   ],
 }
 const customNodes: CustomNote[] = [{
@@ -19,7 +19,7 @@ const customNodes: CustomNote[] = [{
   sequence: 13,
   privilege: "opponent",
 }]
-const result = simulate(questId, allyDeck, undefined, customNodes)
+const result = simulate(questId, allyDeck, undefined, undefined)
 const jsonStr = JSON.stringify(result, undefined, 2)
 fs.writeFileSync("test/result.json", jsonStr, "utf8")
 const a = 1
