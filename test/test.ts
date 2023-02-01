2bad2ab41cdad286fa43c8353c8e391caacaf4e5
MalitsPlus <hua8102259@gmail.com>
2023-02-01 20:39:19 +0900

    test: test.ts chore: gitignore

---
+++ test/test.ts
@@ -0,0 +1,21 @@
+import simulate from "../index";
+import { getDefaultUserCard } from '../satomi/card';
+import { CustomNote, TransDeck } from "../types/trans_types";
+
+const questId = "gvg-quest-019"
+const allyDeck: TransDeck = {
+  name: "test deck",
+  userCards: [
+    { index: 1, card: getDefaultUserCard("card-kor-05-idol-00") },
+    { index: 2, card: getDefaultUserCard("card-mei-05-fest-00") },
+    { index: 3, card: getDefaultUserCard("card-chs-05-flow-00") },
+    { index: 4, card: getDefaultUserCard("card-szk-05-prem-00") },
+    { index: 5, card: getDefaultUserCard("card-mna-05-fest-00") },
+  ],
+}
+const customNodes: CustomNote[] = [{
+  ingamePos: 2,
+  sequence: 13,
+  privilege: "opponent",
+}]
+const result = simulate(questId, allyDeck, undefined, customNodes)
