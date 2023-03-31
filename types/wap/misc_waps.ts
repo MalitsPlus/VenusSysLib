7cbd99c5b9fe564aa3ec43223ab66a6bc2a940b9
MalitsPlus <hello@vbb.im>
2023-03-31 17:02:11 +0900

    fix: omit `cardAppliedStatus` within `LiveBeat`

--- types/wap/misc_waps.ts
+++ types/wap/misc_waps.ts
@@ -1,6 +1,6 @@
 import { LiveBeat } from "../proto/proto_api"
 
-export type WapLiveBeat = Omit<LiveBeat, "score"> & {
+export type WapLiveBeat = Omit<LiveBeat, "score" | "cardAppliedStatus"> & {
   score: number
 }
 
