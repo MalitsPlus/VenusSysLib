df63a669d0c66b271ebcb5a008fe1c3332fd432a
MalitsPlus <50281195+MalitsPlus@users.noreply.github.com>
2023-12-07 17:10:18 +0900

    feat: tests

--- main.ts
+++ main.ts
@@ -1,30 +1,49 @@
-import * as fs from 'fs'
-import simulate from "./index";
+import simulate, { initCard, initCharacter, initQuest, initSetting, initSkill } from "./index";
 import { getDefaultUserCard } from './satomi/card';
 import { CustomNote, TransDeck } from "./types/trans_types"
-import { getAllWapCards } from './db/repository/card_repository';
-import { getAllCharas, getAllGroups } from './db/repository/chara_repository';
+import { getAllGroups } from './db/repository/chara_repository';
+import { initApi } from "./db/dao/api";
+import dotenv from "dotenv"
 
-const questId = "gvg-quest-021"
-const allyDeck: TransDeck = {
-  name: "test deck",
-  userCards: [
-    { index: 1, card: getDefaultUserCard("card-rio-05-fest-00") },
-    { index: 2, card: getDefaultUserCard("card-szk-04-casl-00") },
-    { index: 3, card: getDefaultUserCard("card-ski-05-xmas-00") },
-    { index: 4, card: getDefaultUserCard("card-skr-02-casl-00") },
-    { index: 5, card: getDefaultUserCard("card-szk-05-prem-00") },
-  ],
+async function main() {
+  dotenv.config()
+  if (!process.env.DB_URL || !process.env.DB_SKR) {
+    console.error("DB_URL and DB_SKR must be given")
+    return
+  }
+
+  initApi({
+    url: process.env.DB_URL,
+    sakura: process.env.DB_URL,
+  })
+  await Promise.all([
+    initCard(),
+    initSkill(),
+    initCharacter(),
+    initSetting(),
+    initQuest(),
+  ])
+
+  const questId = "pvp-quest-056"
+  const allyDeck: TransDeck = {
+    name: "test deck",
+    userCards: [
+      { index: 1, card: getDefaultUserCard("card-rio-05-fest-00") },
+      { index: 2, card: getDefaultUserCard("card-szk-04-casl-00") },
+      { index: 3, card: getDefaultUserCard("card-ski-05-xmas-00") },
+      { index: 4, card: getDefaultUserCard("card-skr-02-casl-00") },
+      { index: 5, card: getDefaultUserCard("card-szk-05-prem-00") },
+    ],
+  }
+  const customNodes: CustomNote[] = [{
+    ingamePos: 2,
+    sequence: 13,
+    privilege: "opponent",
+  }]
+  const result = simulate(questId, allyDeck, undefined, undefined)
+
+  const xxx = getAllGroups()
+  const a = 1
 }
-const customNodes: CustomNote[] = [{
-  ingamePos: 2,
-  sequence: 13,
-  privilege: "opponent",
-}]
-const result = simulate(questId, allyDeck, undefined, undefined)
-const jsonStr = JSON.stringify(result, undefined, 2)
-fs.writeFileSync("test/result.json", jsonStr, "utf8")
 
-const xxx = getAllGroups()
-
-const a = 1
+main()
--- test/test.ts
+++ test/test.ts
@@ -1,21 +1,46 @@
-import simulate from "../index";
+import { initApi } from "../db/dao/api";
+import simulate, { initCard, initCharacter, initQuest, initSetting, initSkill } from "../index";
 import { getDefaultUserCard } from '../satomi/card';
 import { CustomNote, TransDeck } from "../types/trans_types";
+import dotenv from "dotenv"
 
-const questId = "gvg-quest-019"
-const allyDeck: TransDeck = {
-  name: "test deck",
-  userCards: [
-    { index: 1, card: getDefaultUserCard("card-kor-05-idol-00") },
-    { index: 2, card: getDefaultUserCard("card-mei-05-fest-00") },
-    { index: 3, card: getDefaultUserCard("card-chs-05-flow-00") },
-    { index: 4, card: getDefaultUserCard("card-szk-05-prem-00") },
-    { index: 5, card: getDefaultUserCard("card-mna-05-fest-00") },
-  ],
+async function test() {
+  dotenv.config()
+  if (!process.env.DB_URL || !process.env.DB_SKR) {
+    console.error("DB_URL and DB_SKR must be given.")
+    return
+  }
+
+  initApi({
+    url: process.env.DB_URL,
+    sakura: process.env.DB_SKR,
+  })
+
+  await Promise.all([
+    initCard(),
+    initSkill(),
+    initCharacter(),
+    initSetting(),
+    initQuest(),
+  ])
+
+  const questId = "gvg-quest-019"
+  const allyDeck: TransDeck = {
+    name: "test deck",
+    userCards: [
+      { index: 1, card: getDefaultUserCard("card-kor-05-idol-00") },
+      { index: 2, card: getDefaultUserCard("card-mei-05-fest-00") },
+      { index: 3, card: getDefaultUserCard("card-chs-05-flow-00") },
+      { index: 4, card: getDefaultUserCard("card-szk-05-prem-00") },
+      { index: 5, card: getDefaultUserCard("card-mna-05-fest-00") },
+    ],
+  }
+  const customNodes: CustomNote[] = [{
+    ingamePos: 2,
+    sequence: 13,
+    privilege: "opponent",
+  }]
+  const result = simulate(questId, allyDeck, undefined, customNodes)
 }
-const customNodes: CustomNote[] = [{
-  ingamePos: 2,
-  sequence: 13,
-  privilege: "opponent",
-}]
-const result = simulate(questId, allyDeck, undefined, customNodes)
+
+test()
